255406fb0d47bff6dbbe35678ddab63965338971
Kabeer <stoiccoolio@gmail.com>
2024-10-05 00:03:05 +0500

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose'); // Import Mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json()); // To parse JSON bodies
 
+// Connect to MongoDB with direct password
+const dbPassword = 'strongphp'; // Your MongoDB password
+const uri = `mongodb+srv://fullstack:${dbPassword}@fullstack.ysh1o.mongodb.net/?retryWrites=true&w=majority&appName=fullstack`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 // Simple route to test if server is running
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+
 app.get('/test', (req, res) => {
-  res.send("kabeeri s rubning")
+  res.send("kabeeri is running");
 });
+
 // Starting the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ frontend/src/components/Kabeer.tsx
@@ -0,0 +1,1 @@
+// strongphp is the db password
